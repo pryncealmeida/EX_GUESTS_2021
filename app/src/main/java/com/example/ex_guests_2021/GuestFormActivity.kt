2d1db6042251b4fe2f473d10e338f26c2dd15f29
pryncealmeida <pryncealmeida@gmail.com>
2021-01-11 14:54:43 -0300

    Commit 11012021 1454

--- app/src/main/java/com/example/ex_guests_2021/GuestFormActivity.kt
+++ app/src/main/java/com/example/ex_guests_2021/GuestFormActivity.kt
@@ -2,10 +2,33 @@ package com.example.ex_guests_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
+import com.example.ex_guests_2021.ui.GuestFormViewModel
+import kotlinx.android.synthetic.main.activity_guest_form.*
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mViewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guest_form)
+
+        mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        setListeners()
+    }
+
+    private fun setListeners(){
+    button_save.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+     val id = v.id
+     if(id == R.id.button_save){
+
+     }
     }
 }
---
+++ app/src/main/java/com/example/ex_guests_2021/ui/GuestFormViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.ex_guests_2021.ui
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel : ViewModel() {
+
+
+
+}
